4650a4e032a7c714976197d74347c8e12e17c3b4
Aamir Zuhail Basheer <132408385+amruazb@users.noreply.github.com>
2024-02-21 11:21:24 +0400

    Update ft_realloc.c

--- libft/ft_realloc.c
+++ libft/ft_realloc.c
@@ -30,21 +30,21 @@
 //without ft_memcpy
 void *ft_realloc(void *ptr, size_t newsize)
 {
-    char *newptr;
-    size_t cursize;
-    if (ptr == NULL)
-        return malloc(newsize);
-    cursize = sizeof(ptr);
-    if (newsize <= cursize)
-        return ptr;
-    newptr = malloc(newsize);
-    // Copy memory byte by byte
+	char *newptr;
+	size_t cursize;
+	if (ptr == NULL)
+	return (malloc(newsize));
+	cursize = sizeof(ptr);
+	if (newsize <= cursize)
+	return ptr;
+	newptr = malloc(newsize);
+	// Copy memory byte by byte
 	size_t i = 0;
-	while(i < cursize) 
-    {
-        ((char *)newptr)[i] = ((char *)ptr)[i];
+	while(i < cursize)
+	{
+	((char *)newptr)[i] = ((char *)ptr)[i];
 		i++;
-    }
-    free(ptr);
-    return newptr;
+	}
+	free(ptr);
+	return newptr;
 }
